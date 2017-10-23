1abf6b7d7c9c0a324e4c854b2049f82df0a4dff1
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-10-23 19:23:20 +0200

    Trying to marshaling objects...

--- SEW_XML_APP/src/Main.java
+++ SEW_XML_APP/src/Main.java
@@ -1,4 +1,64 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import student.Student;
+import student.Students;
 
 public class Main {
+
+	public static void main(String[] args) throws JAXBException {
+		
+		marshlingExample();
+
+		/*try {
+
+			File file = new File("xml/students.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Students.class);
+
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Students students = (Students) jaxbUnmarshaller.unmarshal(file);
+
+			for (Student s : students.getStudents()) {
+				System.out.println(
+						s.getName() + " " + s.getSurname() + " " + s.getId());
+			}
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}*/
+
+	}
+
+	public static void marshlingExample() throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(Students.class);
+		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+		jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		Students students = new Students();
+		List<Student> ls = new ArrayList<Student>();
+		Student s1 = new Student();
+		s1.setId(1);
+		s1.setName("Pepe");
+		s1.setSurname("Rodolfo Suarez");
+		
+		Student s2 = new Student();
+		s2.setId(2);
+		s2.setName("Laura");
+		s2.setSurname("Martinez Villanueva");
+		
+		ls.add(s1);
+		ls.add(s2);
+		students.setStudents(ls);
+		
+		// Marshal the employees list in console
+		jaxbMarshaller.marshal(students, System.out);
+	}
 
 }
