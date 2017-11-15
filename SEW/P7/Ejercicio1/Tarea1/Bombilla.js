98cc08856bc9ebb10d58e88971a856ea1930effa
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-11-15 10:51:00 +0100

    Lecture 20171115

---
+++ SEW/P7/Ejercicio1/Tarea1/Bombilla.js
@@ -0,0 +1,13 @@
+'use strict';
+class Bombilla {
+
+  mostrar() {
+    $('#miBombilla').show();
+  }
+
+  ocultar() {
+    $('#miBombilla').hide();
+  }
+}
+
+var bombilla = new Bombilla();
---
+++ SEW/P7/Ejercicio1/Tarea1/Tarea1.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+  $('#ocultar').click(function () {
+    $('p').hide();
+  });
+
+  $('#mostrar').click(function () {
+    $('p').show();
+  });
+});
