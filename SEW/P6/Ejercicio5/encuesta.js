b215cf0c6c9a0b436533354e4b5ab8c8050a8a60
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-12-08 16:41:40 +0100

    Finished the Scientific calculator

--- SEW/P6/Ejercicio5/encuesta.js
+++ SEW/P6/Ejercicio5/encuesta.js
@@ -1,11 +1,27 @@
 'use strict';
-class encuesta {
+class Encuesta {
 
   constructor() {
     this.average = 0.0;
   }
 
-  calculateAvr(int i, int j, int x, int y) {
-    average = (i+j+x+y)/4;
+  calculateAvr() {
+    average = (formulario.facilidad.value + formulario.funcionalidad.value) / 2;
+  }
+
+  campoNoNulo(value, fieldName) {
+    if (value == '') {
+      alert('El ' + fieldName + ' es obligatorio.');
+      event.returnValue = false;
+    }
+  }
+
+  validate() {
+    this.campoNoNulo(formulario.nombre.value, 'nombre');
+    this.campoNoNulo(formulario.apellidos.value, 'apellidos');
+    this.campoNoNulo(formulario.edad.value, 'edad');
+    this.campoNoNulo(formulario.email.value, 'email');
+    this.calculateAvr();
   }
 }
+var e = new Encuesta();
